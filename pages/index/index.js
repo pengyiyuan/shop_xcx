8612c0a9ed617f79b07c1576fd53ec400696859a
pengyiyuan <18666644561@163.com>
2020-11-08 23:41:04 +0800

    完成首页布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,21 +15,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // wx.request({
-    //   url: 'http://www.pengyiyuan.com:8000/temp/index/getSwiperData',
-    //   success: (result) => {
-    //     this.setData({
-    //       swiperList: result.data.data
-    //     })
-    //   }
-    // })
    this.getSwiperData();
   },
 
   // 获取轮播图数据
   getSwiperData(){
     request({
-      url: 'http://www.pengyiyuan.com:8000/temp/index/getSwiperData'
+      url: '/temp/index/getSwiperData'
     })
     .then(result => {
       this.setData({
@@ -38,52 +30,6 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
--- request/request.js
+++ request/request.js
@@ -1,7 +1,9 @@
 export const request = (params) => {
   return new Promise((resolve, reject) => {
+    const baseUrl = "http://192.168.31.2:8000";
     wx.request({
       ...params,
+      url: baseUrl + params.url, 
       success: (result) => {
         resolve(result);
       },
